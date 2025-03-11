f9f3019d1121b3bb4982cc2ba3d0a6122e60156b
Enes Bakadur <enesbakadur@outlook.com>
2025-03-11 16:52:18 +0300

    added halls section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/sections/HeroSection';
 import AboutUsSection from '@/components/sections/AboutUsSection';
+import HallsSection from '@/components/sections/HallsSection';
 
 export default function Home() {
     return (
         <main>
             <HeroSection />
             <AboutUsSection />
+            <HallsSection />
         </main>
     );
 }
